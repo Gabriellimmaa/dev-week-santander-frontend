ab895a889d09a0aa30363fcf77d82381e1eb3d83
SEU <gabriellimamoraes@gmail.com>
2021-05-29 02:10:01 -0300

    commit

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'https://bootcamp-dio.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://backend-bootcamp-santander.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
